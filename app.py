2001e52c126945788cf06978642de3fd53091e9e
Patrick566 <patrick.galligan@menloschool.org>
2023-05-09 22:51:18 -0700

    Finished Project

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from models import *
 app = Flask(__name__)
 app.secret_key = "Key"
 
+# Login and Signup page (also option for continuing without logging in)
 @app.route("/", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
@@ -19,7 +20,7 @@ def login():
                 db_session.commit()
                 flash("Welcome " + username + "! You have successfully signed up")
                 session["username"] = username
-                return render_template("search.html")
+                return render_template("search.html", username=session["username"])
             else:
                 flash("Your Username is alread in use, please Login instead")
                 return render_template("home.html")
@@ -29,18 +30,19 @@ def login():
             if db_session.query(User).where((User.user_name == username) & (User.password == password)).first() is not None:
                 flash("Welcome back " + username + "! You have successfully logged in")
                 session["username"] = username
-                return render_template("search.html")
+                return render_template("search.html", username=session["username"])
             else:
                 flash("You Password or Username is incorrect, please Sign up instead", "info")
                 return render_template("home.html")
     if request.method == "GET":
         return render_template("home.html")
 
+# Actual search page with translation functionallity
 @app.route("/search", methods=["GET", "POST"])
 def search():
     if request.method == "GET":
         if "username" in session:
-            return render_template("search.html", username=session["username"])
+            return render_template("search.html", username="")
         else:
             return render_template("search.html")
         
@@ -48,16 +50,26 @@ def search():
         search = request.form["search"]
         definition = db_session.query(Word).where(Word.word == search).first()
         if definition is not None:
-            return render_template("extension.html", definition = definition)
+            return render_template("extension.html", definition = definition, username=session["username"])
         else:
             flash("Word does not exist in our database")
-            return render_template("search.html")
+            return render_template("search.html", username=session["username"])
 
+# List of all words and definitions
 @app.route("/dictionary")
 def dictionary():
     definitions = db_session.query(Word).all()
     print(definitions)
-    return render_template("dictextension.html", definitons = definitions)
+    return render_template("dictextension.html", definitons = definitions, username=session["username"])
+
+# Logout function
+@app.route("/logout")
+def logout():
+    if "username" in session:
+        session.pop("username")
+        return redirect(url_for("login"))
+    else:
+        return redirect(url_for("login"))
 
 
 if __name__ == "__main__":
